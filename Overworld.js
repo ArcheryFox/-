f1504beb7cb366056aafab5bc23d9d3e97b674c0
ArcheryFox <archeryfox2005@gmail.com>
2022-04-01 21:19:42 +0300

    Revert "CANCEL PREV COMM"
    This reverts commit a385de0ef9b4f1acca2091aa04297b5282e26044.

--- Overworld.js
+++ Overworld.js
@@ -1,4 +1,14 @@
 class Overworld {
+<<<<<<< Updated upstream
+    constructor(config) {
+        this.element = config.element;
+        this.canvas = this.element.querySelector(`.conteinerCanv`);
+        this.ctx = this.canvas.getContext('2d');
+        this.map = null;
+        // this.canvas.height = 300;
+        // this.canvas.width = 400;
+    } 
+=======
   constructor(config) {
       this.element = config.element;
       this.canvas = this.element.querySelector(`.conteinerCanv`);
@@ -7,6 +17,7 @@ class Overworld {
       // this.canvas.height = 300;
       // this.canvas.width = 400;
   }
+>>>>>>> Stashed changes
 
   startGameLoop() {
       const step = () => {
@@ -15,6 +26,25 @@ class Overworld {
   
           const cameraPerson = this.map.gameObjects.hero;
 
+<<<<<<< Updated upstream
+                Object.values(this.map.gameObjects).forEach(object => {
+                    object.update({
+                      arrow: this.directionInput.direction,
+                      map: this.map,
+                    });
+                })
+          //Draw Lower layer
+          this.map.drawLowerImage(this.ctx, cameraPerson);
+    
+          //Draw Game Objects
+          Object.values(this.map.gameObjects).forEach(object => {
+            
+            object.sprite.draw(this.ctx, cameraPerson);
+          })
+    
+          //Draw Upper layer
+          this.map.drawUpperImage(this.ctx, cameraPerson);
+=======
               Object.values(this.map.gameObjects).forEach(object => {
                   object.update({
                     arrow: this.directionInput.direction
@@ -25,6 +55,7 @@ class Overworld {
   
         //Draw Game Objects
         Object.values(this.map.gameObjects).forEach(object => {
+>>>>>>> Stashed changes
           
           object.sprite.draw(this.ctx, cameraPerson);
         })
--- direction.js
+++ direction.js
@@ -1,4 +1,42 @@
 class DirectionInput {
+<<<<<<< Updated upstream
+    constructor() {
+      this.heldDirections = [];
+  
+      this.map = {
+        "ArrowUp": "up",
+        "KeyW": "up",
+        "ArrowDown": "down",
+        "KeyS": "down",
+        "ArrowLeft": "left",
+        "KeyA": "left",
+        "ArrowRight": "right",
+        "KeyD": "right", 
+      }
+    }
+  
+    get direction() {
+      return this.heldDirections[0]
+    }
+  
+    init() {
+      document.addEventListener("keydown", e => {
+        const dir = this.map[e.code];
+        if (dir && this.heldDirections.indexOf(dir) === -1) {
+          this.heldDirections.unshift(dir);
+        //   console.log("a")
+        }
+      });
+      document.addEventListener("keyup", e => {
+        const dir = this.map[e.code];
+        const index = this.heldDirections.indexOf(dir);
+        if (index > -1) {
+          this.heldDirections.splice(index, 1);
+        //   console.log('b')
+        } 
+      })
+  
+=======
   constructor() {
     this.heldDirections = [];
 
@@ -11,6 +49,7 @@ class DirectionInput {
       "KeyA": "left",
       "ArrowRight": "right",
       "KeyD": "right",
+>>>>>>> Stashed changes
     }
   }
 
